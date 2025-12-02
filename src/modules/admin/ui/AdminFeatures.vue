80d6377427089f106d5af132519f9d82361aea00
Minh Dong <70119705+minhcu@users.noreply.github.com>
2025-12-02 16:26:18 +0700

    feat: popup layout (#1)

---
+++ src/modules/admin/ui/AdminFeatures.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+import { Card } from 'ant-design-vue';
+</script>
+
+<template>
+  <Card title="Admin tool">
+    <p>Admin tool content goes here.</p>
+  </Card>
+</template>
---
+++ src/modules/shopify/ui/ShopifyFeatures.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+import { Card } from 'ant-design-vue';
+</script>
+
+<template>
+  <Card title="Shopify tool">
+    <p>Shopify tool content goes here.</p>
+  </Card>
+</template>
