2d795870ab805f0f7b4759e79fcc479f37417719
minhcu <minhdong1831999@gmail.com>
2025-12-03 16:43:01 +0700

    feat: USF features

--- src/modules/usf/ui/UsfFeatures.vue
+++ src/modules/usf/ui/UsfFeatures.vue
@@ -1,9 +1,15 @@
 <script lang="ts" setup>
-import { Card } from 'ant-design-vue';
+import { Button, Card, Space } from 'ant-design-vue';
+import { UnlockOutlined, ExportOutlined } from '@ant-design/icons-vue';
+import { h } from 'vue';
+import { loginUsf, previewCurrentTheme } from '@/modules/usf/api'
 </script>
 
 <template>
   <Card title="Usf features">
-    <p>Usf features content goes here.</p>
+    <Space class="w-full" direction="vertical">
+      <Button class="w-full" type="primary" :icon="h(UnlockOutlined)" @click="loginUsf">Login USF</Button>
+      <Button class="w-full" type="primary" :icon="h(ExportOutlined)" @click="previewCurrentTheme">Preview Current Theme</Button>
+    </Space>
   </Card>
 </template>
--- src/shared/store.ts
+++ src/shared/store.ts
@@ -2,21 +2,27 @@ import { defineStore } from 'pinia'
 import { reactive, ref } from 'vue'
 
 export type Credentials = {
+  emailLookupUrl: string
   adminUsername: string
   adminPassword: string
-  usfPassword: string
-  stagingUsfPassword: string
-  emailLookupUrl: string
   usfAppUrl: string
+  usfPassword: string
+  usfStagingAppUrl: string
+  usfStagingPassword: string
+  usfStagingUsername: string
 }
 export const useCredentialsStore = defineStore('credentials', () => {
   const credentials = reactive<Credentials>({
+    emailLookupUrl: '',
     adminUsername: '',
     adminPassword: '',
-    usfPassword: '',
-    stagingUsfPassword: '',
-    emailLookupUrl: '',
+
     usfAppUrl: '',
+    usfPassword: '',
+
+    usfStagingAppUrl: '',
+    usfStagingUsername: '',
+    usfStagingPassword: '',
   })
 
   chrome.storage.local.get('credentials', (result: { credentials?: typeof credentials }) => {
@@ -38,19 +44,19 @@ export const useCredentialsStore = defineStore('credentials', () => {
 
 export type StoreCredentials = Record<string, string>
 export const useStoreCredentialsStore = defineStore('storeCredentials', () => {
-  const storeCredentials = ref<StoreCredentials>({})
+  const stores = ref<StoreCredentials>({})
 
   chrome.storage.local.get('storeCredentials', (result: { storeCredentials?: StoreCredentials }) => {
     if (!result.storeCredentials) return
-    Object.assign(storeCredentials, result.storeCredentials)
+    stores.value = result.storeCredentials
   })
 
   function updateStoreCredentials() {
-    chrome.storage.local.set({ storeCredentials })
+    chrome.storage.local.set({ storeCredentials: stores.value })
   }
 
   return {
-    storeCredentials,
+    stores,
     updateStoreCredentials,
   }
 })
