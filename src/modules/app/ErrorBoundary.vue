2b63bac4da114d96b7eaa29abb505f31e6587f22
minhcu <minhdong1831999@gmail.com>
2025-12-05 15:54:00 +0700

    feat: add chrome extension listener

--- src/modules/app/ErrorBoundary.vue
+++ src/modules/app/ErrorBoundary.vue
@@ -1,5 +1,33 @@
 <script lang="ts" setup>
-import { contextHolder } from '../../shared/error';
+import { useStoreCredentialsStore } from '@/shared/store';
+import { contextHolder, notifyError } from '../../shared/error';
+
+const storeCredentialsStore = useStoreCredentialsStore();
+chrome.runtime.onMessage.addListener((message: {
+  type: string;
+  payload: string;
+}) => {
+  switch (message.type) {
+    case "addStoreCredential":
+      const [url, email] = message.payload.split(":")
+      if (!url || !email || !email.includes('@')) {
+        notifyError('Received invalid store credential data');
+        return;
+      }
+
+      storeCredentialsStore.stores = {
+        ...storeCredentialsStore.stores,
+        [url]: email
+      }
+
+      storeCredentialsStore.updateStoreCredentials();
+
+      break;
+    default:
+      notifyError('Unknown message type received in ErrorBoundary');
+      return;
+  }
+});
 </script>
 
 <template>
