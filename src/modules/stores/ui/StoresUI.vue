42dc5214414e6a2db850fba09b2b1d647bf80e30
minhcu <minhdong1831999@gmail.com>
2025-12-08 16:04:41 +0700

    feat: stores list management

--- src/modules/stores/ui/StoresUI.vue
+++ src/modules/stores/ui/StoresUI.vue
@@ -1,20 +1,51 @@
 <script lang="ts" setup>
 import { Button, Modal, Space } from 'ant-design-vue';
 import { DatabaseOutlined } from '@ant-design/icons-vue';
-import { h, ref } from 'vue';
+import { computed, h, ref } from 'vue';
+import { useStoreCredentialsStore } from '@/shared/store';
+import StoresImport from './StoresImport.vue';
+import StoresList from './StoresList.vue';
 
 const isOpen = ref(false);
+
+const storesCredentials = useStoreCredentialsStore();
+
+const columns = [
+  {
+    title: 'Store ID',
+    dataIndex: 'storeId',
+    key: 'storeId',
+  },
+  {
+    title: 'Email',
+    dataIndex: 'email',
+    key: 'email',
+  },
+  {
+    title: 'Operation',
+    dataIndex: 'operation',
+    key: 'operation',
+  },
+];
+const tableData = computed(() => {
+  const list = storesCredentials.stores;
+
+  return Object.keys(list).map((store) => ({
+    key: store,
+    storeId: store,
+    email: list[store]
+  }))
+});
 </script>
 
 <template>
   <Button type="default" :icon="h(DatabaseOutlined)" @click="isOpen = true" style="width: 100%">View
     Stores List</Button>
-  <Modal title="Settings" v-model:open="isOpen">
-    <Space direction="vertical" style="width: 100%; justify-content: space-between;">
-      <div style="width: 100%;">
-        <h3>Stores List</h3>
-        <p>Stores list content goes here.</p>
-      </div>
+  <Modal title="Settings" v-model:open="isOpen" width="500">
+    <Space class="w-full" direction="vertical">
+      <StoresImport />
+
+      <StoresList :columns="columns" :tableData="tableData" @deleteStore="storesCredentials.deleteStore" />
     </Space>
   </Modal>
 </template>
