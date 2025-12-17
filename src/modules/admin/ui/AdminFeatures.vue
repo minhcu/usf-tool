a02706e60974be0935ff019f30abf555c77c10e0
minhcu <minhdong1831999@gmail.com>
2025-12-17 13:51:07 +0700

    update: handling admin error and loading

--- src/modules/admin/ui/AdminFeatures.vue
+++ src/modules/admin/ui/AdminFeatures.vue
@@ -2,9 +2,26 @@
 import { ExportOutlined, UnlockOutlined } from '@ant-design/icons-vue';
 import { Button, Card, Input, Space } from 'ant-design-vue';
 import { h, ref } from 'vue';
-import { getStoreEmailFromUrl, loginAdminTool, openAdminTool } from '../shared/api';
+import { getStoreEmailFromUrl, loginAdminTool, openAdminTool } from '../shared';
+import { notifyError } from '@/shared/error';
 
 const email = ref('');
+const loadingState = ref<Record<string, boolean>>({
+  getStoreUrl: false,
+  openAdmin: false,
+  loginAdmin: false
+});
+
+async function handleAction(actionKey: string, callback: () => void | Promise<void>) {
+  loadingState.value[actionKey] = true;
+  try {
+    await callback();
+  } catch (error) {
+    notifyError((error as Error).message);
+  } finally {
+    loadingState.value[actionKey] = false;
+  }
+}
 </script>
 
 <template>
@@ -12,11 +29,18 @@ const email = ref('');
     <Space class="w-full" direction="vertical">
       <Space class="w-full" direction="vertical">
         <Input class="w-full" placeholder="Enter store URL" v-model:value="email" />
-        <Button class="w-full mt-2" type="primary" :icon="h(ExportOutlined)" @click="getStoreEmailFromUrl(email)">Get Store Url</Button>
+        <Button class="w-full mt-2" type="primary" :icon="h(ExportOutlined)"
+          @click="handleAction('getStoreUrl', () => getStoreEmailFromUrl(email))"
+          :loading="loadingState.getStoreUrl">Get Store Url</Button>
       </Space>
+
       <Space.Compact class="w-full">
-        <Button class="w-full" type="primary" :icon="h(ExportOutlined)" @click="openAdminTool">Open Admin Tool</Button>
-        <Button class="w-full" type="primary" :icon="h(UnlockOutlined)" @click="loginAdminTool">Login Admin Tool</Button>
+        <Button class="w-full" type="primary" :icon="h(ExportOutlined)"
+          @click="handleAction('openAdmin', openAdminTool)" :loading="loadingState.openAdmin">Open Admin Tool</Button>
+
+        <Button class="w-full" type="primary" :icon="h(UnlockOutlined)"
+          @click="handleAction('loginAdmin', loginAdminTool)" :loading="loadingState.loginAdmin">Login Admin
+          Tool</Button>
       </Space.Compact>
     </Space>
   </Card>
